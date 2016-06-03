32fb198bb09c8ba89bee6b176fbf27f4054cba7f
Francis Gómez <fran_jo@hotmail.com>
2016-06-03 11:36:09 +0200

    New main and new classes for designing and building

---
+++ src/cim2model/BetaFactoryCIMOD.java
@@ -0,0 +1,182 @@
+package cim2model;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import cim2model.cim.*;
+import cim2model.cim.map.*;
+import cim2model.cim.map.ipsl.base.*;
+import cim2model.cim.map.ipsl.branches.*;
+import cim2model.cim.map.ipsl.buses.*;
+import cim2model.cim.map.ipsl.connectors.*;
+import cim2model.cim.map.ipsl.loads.*;
+import cim2model.cim.map.ipsl.machines.*;
+import cim2model.cim.map.ipsl.transformers.*;
+import cim2model.modelica.*;
+import cim2model.modelica.ipsl.machines.IPSLMachine;
+
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+
+public class BetaFactoryCIMOD 
+{
+	public static void main(String[] args) 
+	{
+		Map<Resource, RDFNode> components;
+		String [] cimClassResource;
+		String _cimSource= args[0];
+		FactoryDesigner cartografo;
+		FactoryBuilder constructor;
+		
+		cartografo= new FactoryDesigner(_cimSource);
+		constructor= new FactoryBuilder(args[1]);
+		components= cartografo.load_CIMModel();
+		for (Resource key : components.keySet())
+		{	
+			cimClassResource= cartografo.get_CIMComponentName(key);
+			/* subjectResource[0] is the rfd_id, subjectResource[1] is the CIM name */
+			if (cimClassResource[1].equals("Terminal"))
+			{
+				String [] equipmentResource, topologyResource;
+				PwPinMap mapTerminal= 
+						cartografo.create_TerminalModelicaMap(key, "./res/map/cim_iteslalibrary_pwpin.xml", cimClassResource);
+				MOConnector mopin= constructor.create_PinConnector(mapTerminal);
+				/* after loading terminal, load the resource connected to it, aka, ConductingEquipment */
+				equipmentResource= cartografo.get_CIMComponentName(
+						cartografo.get_CurrentConnectionMap().getConductingEquipment());
+				topologyResource= cartografo.get_CIMComponentName(
+						cartografo.get_CurrentConnectionMap().getTopologicalNode());
+				/* According to CIM Composer, SynchMachine has one terminal */
+				if (equipmentResource[1].equals("SynchronousMachine"))
+				{
+					IPSLMachine momachine= null;
+					String machineType= cartografo.typeOfSynchronousMachine(
+							cartografo.get_CurrentConnectionMap().getConductingEquipment());
+					if (machineType.equals("GENCLS")) {
+						GENCLSMap mapSyncMach= cartografo.create_GENCLSModelicaMap(
+								cartografo.get_CurrentConnectionMap().getConductingEquipment(), 
+								"./res/map/cim_iteslalibrary_gencls.xml", equipmentResource);
+						momachine= constructor.create_MachineComponent(mapSyncMach);
+					}
+					if (machineType.equals("GENROU")) {
+						GENROUMap mapSyncMach= cartografo.create_GENROUModelicaMap(
+								cartografo.get_CurrentConnectionMap().getConductingEquipment(), 
+								"./res/map/cim_iteslalibrary_genrou.xml", equipmentResource);
+						momachine= constructor.create_MachineComponent(mapSyncMach);
+					}
+					if (machineType.equals("GENSAL")){
+						GENSALMap mapSyncMach= cartografo.create_GENSALModelicaMap(
+								cartografo.get_CurrentConnectionMap().getConductingEquipment(), 
+								"./res/map/cim_iteslalibrary_gensal.xml", equipmentResource);
+						momachine= constructor.create_MachineComponent(mapSyncMach);
+					}
+					if (machineType.equals("GENROE")){
+						GENROEMap mapSyncMach= cartografo.create_GENROEModelicaMap(
+								cartografo.get_CurrentConnectionMap().getConductingEquipment(), 
+								"./res/map/cim_iteslalibrary_genroe.xml", equipmentResource);
+						momachine= constructor.create_MachineComponent(mapSyncMach);
+					}
+					//TODO with the object map, look and create object map for ES, TG and Stab objects
+					//TODO create plant object, name of generator instance name, with genmap, esmap, tgmap and stabmap
+					//genmap can contain ES[0..1], TG[0..1], PSS[0..1]
+					momachine.add_Terminal(mopin);
+					momachine.update_powerFlow(mopin);
+					MOPlant moplanta= new PlantBuilder().machine(momachine).buildPlant();
+					moplanta.add_Terminal(mopin);
+					constructor.add_plantNetwork(moplanta);
+				}
+				/* According to CIM Composer, EnergyConsumer has one terminal */
+				if (equipmentResource[1].equals("EnergyConsumer"))
+				{
+					LoadMap mapEnergyC= cartografo.create_LoadModelicaMap(
+							cartografo.get_CurrentConnectionMap().getConductingEquipment(), 
+							"./res/map/cim_iteslalibrary_load.xml", equipmentResource);
+					MOClass moload= constructor.create_LoadComponent(mapEnergyC);
+					moload.add_Terminal(mopin);
+					moload.update_powerFlow(mopin);
+					constructor.add_equipmentNetwork(moload);
+				}
+				/* According to CIM Composer, ACLineSegment has two terminals */
+				if (equipmentResource[1].equals("ACLineSegment"))
+				{
+					MOClass moline= constructor.get_equipmentNetwork(equipmentResource[0]);
+					if (moline!= null)
+					{/* condition to check if the line already exist in the model, true, add the second terminal */
+						moline.add_Terminal(mopin);
+					}
+					else 
+					{/* false, create map of the line and add the first terminal */
+						PwLineMap mapACLine= cartografo.create_LineModelicaMap(
+								cartografo.get_CurrentConnectionMap().getConductingEquipment(), 
+								"./res/map/cim_iteslalibrary_pwline.xml", equipmentResource);
+						moline= constructor.create_LineComponent(mapACLine);
+						moline.add_Terminal(mopin);
+						constructor.add_equipmentNetwork(moline);
+					}
+				}
+				/* According to CIM Composer, TN has 1..N terminals */
+				if (topologyResource[1].equals("TopologicalNode"))
+				{
+					MOClass mobus= constructor.get_equipmentNetwork(topologyResource[0]);
+					if (mobus!= null)
+					{/* condition to check if the line already exist in the model, true, add the second terminal */
+						mobus.add_Terminal(mopin);
+					}
+					else
+					{/* false, create map of the line and add the first terminal */
+						PwBusMap mapTopoNode= cartografo.create_BusModelicaMap(
+								cartografo.get_CurrentConnectionMap().getTopologicalNode(), 
+										"./res/map/cim_iteslalibrary_pwbus.xml", topologyResource);
+						mobus= constructor.create_BusComponent(mapTopoNode);
+						mobus.add_Terminal(mopin);
+						constructor.add_equipmentNetwork(mobus);
+					}
+				}
+			}
+			if (cimClassResource[1].equals("PowerTransformerEnd"))
+			{
+				String [] transformerResource, terminalResource;
+				CIMTransformerEnd transformerEnd= cartografo.create_TransformerModelicaMap(key, 
+								"./res/map/cim_iteslalibrary_twowindingtransformer.xml", 
+								cimClassResource);
+				TwoWindingTransformerMap mapPowerTrans= transformerEnd.get_TransformerMap();
+				transformerResource= cartografo.get_CIMComponentName(transformerEnd.get_PowerTransformerMap());
+				terminalResource= cartografo.get_CIMComponentName(transformerEnd.get_TerminalMap());
+				PwPinMap mapTerminal= cartografo.create_TerminalModelicaMap(transformerEnd.get_TerminalMap(), 
+								"./res/map/cim_iteslalibrary_pwpin.xml", terminalResource);
+				/*Create the terminal object associated with this PowerTransformerEnd*/
+				MOConnector mopin= constructor.create_PinConnector(mapTerminal);
+				/*Create additional attribute for the ratio tap changer associated with the PowerTransformerEnd*/
+				ArrayList<MOAttribute> moPowTransEnd= constructor.create_AttTransformerEnd(mapPowerTrans);
+				/*check if the PowerTransformer is already in the network*/
+				MOClass moTransformer= constructor.get_equipmentNetwork(transformerResource[0]);
+				if (moTransformer!= null) {
+					moTransformer.add_Terminal(mopin);
+					for(MOAttribute moparam: moPowTransEnd)
+						moTransformer.add_Attribute(moparam);
+				}
+				else {
+					moTransformer= constructor.create_TransformerComponent(mapPowerTrans);
+					moTransformer.add_Terminal(mopin);
+					for(MOAttribute moparam: moPowTransEnd)
+						moTransformer.add_Attribute(moparam);
+					constructor.add_equipmentNetwork(moTransformer);
+				}
+				moPowTransEnd.clear(); moPowTransEnd= null;
+			}
+//			if (cimClassResource[1].equals("Fault"))
+//			{
+//				System.out.println("rfd_id: "+ cimClassResource[0] + " cim name: "+ cimClassResource[1]);
+//				PwFaultMap mapFault= cartografo.create_FaultModelicaMap(key, "./res/map/cim_iteslalibrary_pwfault.xml", cimClassResource);
+//				MOClass mofault= constructor.create_FaultComponent(mapFault);
+//				
+//				constructor.add_deviceNetwork(mofault);
+//				System.out.println(mofault.to_ModelicaClass());
+//				System.out.println(mofault.to_ModelicaInstance());
+//			}
+
+		}
+		constructor.connect_Components(cartografo.get_ConnectionMap());
+		constructor.save_ModelicaFile(constructor.get_Network().to_ModelicaClass());
+	}
+}
---
+++ src/cim2model/FactoryBuilder.java
@@ -0,0 +1,517 @@
+package cim2model;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import cim2model.cim.map.*;
+import cim2model.cim.map.ipsl.base.*;
+import cim2model.cim.map.ipsl.branches.*;
+import cim2model.cim.map.ipsl.buses.*;
+import cim2model.cim.map.ipsl.connectors.*;
+import cim2model.cim.map.ipsl.loads.*;
+import cim2model.cim.map.ipsl.machines.*;
+import cim2model.cim.map.ipsl.transformers.*;
+import cim2model.modelica.*;
+import cim2model.modelica.ipsl.branches.PwLine;
+import cim2model.modelica.ipsl.machines.IPSLMachine;
+
+public class FactoryBuilder 
+{
+	private MONetwork powsys;
+	private MOClass _currentEquipment, _currentNode;
+	private String _currentIDEquipment, _currentIDNode;
+	
+	public FactoryBuilder(String _network)
+	{
+		powsys= new MONetwork(_network);
+		this._currentEquipment= null;
+		this._currentIDEquipment= "";
+		this._currentNode= null;
+		this._currentIDNode= "";
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public MONetwork get_Network() {
+		return this.powsys;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public MOClass get_CurrentEquipment(){
+		return _currentEquipment;
+	}
+	/**
+	 * 
+	 * @param _device
+	 * @param _id
+	 */
+	public void set_CurrentEquipment(MOClass _device, String _id){
+		this._currentEquipment= _device;
+		this._currentIDEquipment= _id;
+	}
+	/**
+	 * 
+	 * @param _id
+	 * @return
+	 */
+	public boolean exist_CurrentEquipment(String _id)
+	{
+		if (this._currentIDEquipment.equals(_id))
+			return true;
+		else
+			return false;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public MOClass get_CurrentNode(){
+		return _currentNode;
+	}
+	/**
+	 * 
+	 * @param _device
+	 * @param _id
+	 */
+	public void set_CurrentNode(MOClass _device, String _id){
+		this._currentNode= _device;
+		this._currentIDNode= _id;
+	}
+	/**
+	 * 
+	 * @param _id
+	 * @return
+	 */
+	public boolean exist_CurrentNode(String _id)
+	{
+		if (this._currentIDNode.equals(_id))
+			return true;
+		else
+			return false;
+	}
+	
+	public MOConnector create_PinConnector(PwPinMap _terminalMap)
+	{
+		MOConnector pin= new MOConnector(_terminalMap.getName());
+		ArrayList<AttributeMap> mapAttList= 
+				(ArrayList<AttributeMap>)_terminalMap.getMapAttribute();
+		Iterator<AttributeMap> imapAttList= mapAttList.iterator();
+		imapAttList= mapAttList.iterator();
+		AttributeMap current;
+		while (imapAttList.hasNext()) {
+			current= imapAttList.next();
+			if (current.getCimName().equals("IdentifiedObject.name")){
+				pin.set_InstanceName(current.getContent());
+			}
+			else{
+				MOAttribute variable= new MOAttribute();
+				variable.set_Name(current.getMoName());
+				variable.set_Value(current.getContent());
+				variable.set_Variability(current.getVariability());
+				variable.set_Visibility(current.getVisibility());
+				variable.set_Flow(Boolean.valueOf(current.getFlow()));
+				pin.set_Attribute(variable);
+			}
+		}
+		pin.set_Stereotype(_terminalMap.getStereotype());
+		pin.set_Package(_terminalMap.getPackage());
+		//for internal identification only
+		pin.set_RfdId(_terminalMap.getRfdId());
+		
+		return pin;
+	}
+	
+	/**
+	 * 
+	 * @param _component
+	 */
+	public void add_equipmentNetwork(MOClass _component)
+	{
+		Iterator<MOClass> iComponents= this.powsys.get_Equipment().iterator();
+		boolean exists= false;
+		while (!exists && iComponents.hasNext()){
+			exists= iComponents.next().get_InstanceName().equals(_component.get_InstanceName());
+		}
+		if (!exists)
+			this.powsys.add_Component(_component);
+	}
+
+	/**
+	 * 
+	 * @param _rfdId
+	 * @return
+	 */
+	public MOClass get_equipmentNetwork(String _rfdId)
+	{
+		MOClass current= new MOClass("void");
+		Iterator<MOClass> iComponents;
+		
+		iComponents= this.powsys.get_Equipment().iterator();
+		boolean exists= false;
+		while (!exists && iComponents.hasNext()){
+			current= iComponents.next();
+			exists= current.get_RfdId().equals(_rfdId);
+		}
+		if (exists)
+			return current;
+		else
+			return null;
+	}
+	
+	public IPSLMachine create_MachineComponent(SynchronousMachineMap _mapSyncMach)
+	{
+		IPSLMachine syncMach= new IPSLMachine(_mapSyncMach.getName());
+		Iterator<AttributeMap> imapAttList= _mapSyncMach.getMapAttribute().iterator();
+		AttributeMap current;
+		while (imapAttList.hasNext()) {
+			current= imapAttList.next();
+			if (current.getCimName().equals("IdentifiedObject.name")){
+				syncMach.set_InstanceName(current.getContent());
+			}
+			else{
+				MOAttribute variable= new MOAttribute();
+				variable.set_Name(current.getMoName());
+				if (current.getContent()== null)
+					variable.set_Value("0");
+				else
+					variable.set_Value(current.getContent());
+				variable.set_Variability(current.getVariability());
+				variable.set_Visibility(current.getVisibility());
+				variable.set_Flow(Boolean.valueOf(current.getFlow()));
+				syncMach.add_Attribute(variable);
+			}
+		}
+		syncMach.set_Stereotype(_mapSyncMach.getStereotype());
+		syncMach.set_Package(_mapSyncMach.getPackage());
+		//for internal identification only
+		syncMach.set_RfdId(_mapSyncMach.getRfdId());
+		
+		return syncMach;
+	}
+	
+	public MOClass create_LoadComponent(LoadMap _mapEnergyC)
+	{
+		MOClass pwLoad= new MOClass(_mapEnergyC.getName());
+		MOAttributeComplex complejo= null;
+		Iterator<AttributeMap> imapAttList= ((ArrayList<AttributeMap>)_mapEnergyC.getMapAttribute()).iterator();
+		AttributeMap current;
+		while (imapAttList.hasNext()) {
+			current= imapAttList.next();
+			if (current.getCimName().equals("IdentifiedObject.name")){
+				pwLoad.set_InstanceName(current.getContent());
+			}
+			else {
+				if (current.getDatatype().equals("Complex")) {
+					String nombre= current.getMoName().split("[.]")[0];
+					String parte= current.getMoName().split("[.]")[1];
+					if (!pwLoad.exist_Attribute(nombre))
+						complejo= new MOAttributeComplex();
+					else
+						complejo= (MOAttributeComplex)pwLoad.get_Attribute(nombre);
+					complejo.set_Name(nombre);
+					if (parte.equals("re")){
+						complejo.set_Real(current.getContent());
+						pwLoad.add_Attribute(complejo);
+					}
+					else {
+						complejo.set_Imaginary(current.getContent());
+						complejo.set_Datatype(current.getDatatype());
+						complejo.set_Variability(current.getVariability());
+						complejo.set_Visibility(current.getVisibility());
+						complejo.set_Flow(Boolean.valueOf(current.getFlow()));
+					}
+				}
+				else {
+					MOAttribute variable= new MOAttribute();
+					variable.set_Name(current.getMoName());
+					if (current.getContent()== null)
+						variable.set_Value("0");
+					else
+						variable.set_Value(current.getContent());
+					variable.set_Datatype(current.getDatatype());
+					variable.set_Variability(current.getVariability());
+					variable.set_Visibility(current.getVisibility());
+					variable.set_Flow(Boolean.valueOf(current.getFlow()));
+					pwLoad.add_Attribute(variable);
+				}
+			}
+		}
+		pwLoad.set_Stereotype(_mapEnergyC.getStereotype());
+		pwLoad.set_Package(_mapEnergyC.getPackage());
+		//for internal identification only
+		pwLoad.set_RfdId(_mapEnergyC.getRfdId());
+		
+		return pwLoad;
+	}
+	
+	public MOClass create_LineComponent(PwLineMap _mapACLine)
+	{
+		PwLine pwline= new PwLine(_mapACLine.getName());
+		ArrayList<AttributeMap> mapAttList= 
+				(ArrayList<AttributeMap>)_mapACLine.getMapAttribute();
+		Iterator<AttributeMap> imapAttList= mapAttList.iterator();
+		AttributeMap current;
+		while (imapAttList.hasNext()) {
+			current= imapAttList.next();
+			if (current.getCimName().equals("IdentifiedObject.name")){
+				pwline.set_InstanceName(current.getContent());
+			}
+			else{
+				MOAttribute variable= new MOAttribute();
+				variable.set_Name(current.getMoName());
+				if (current.getContent()== null)
+					variable.set_Value("0");
+				else
+					variable.set_Value(current.getContent());
+				variable.set_Variability(current.getVariability());
+				variable.set_Visibility(current.getVisibility());
+				variable.set_Flow(Boolean.valueOf(current.getFlow()));
+				pwline.add_Attribute(variable);
+			}
+		}
+		pwline.set_Stereotype(_mapACLine.getStereotype());
+		pwline.set_Package(_mapACLine.getPackage());
+		//for internal identification only
+		pwline.set_RfdId(_mapACLine.getRfdId());
+		
+		return pwline;
+	}
+	
+	public MOClass create_TransformerComponent(TwoWindingTransformerMap _mapPowTrans)
+	{
+		MOClass twtransformer= new MOClass(_mapPowTrans.getName());
+		AttributeMap current;
+		
+		Iterator<AttributeMap> imapAttList= _mapPowTrans.getMapAttribute().iterator();
+		while (imapAttList.hasNext()) {
+			current= imapAttList.next();
+			if (!current.getCimName().equals("TransformerEnd.endNumber") && 
+				!current.getCimName().equals("RatioTapChanger.stepVoltageIncrement") &&
+				!current.getCimName().equals("PowerTransformerEnd.ratedU"))
+			{
+				if (current.getCimName().equals("IdentifiedObject.name"))
+					twtransformer.set_InstanceName(current.getContent());
+				else
+				{
+					MOAttribute variable= new MOAttribute();
+					variable.set_Name(current.getMoName());
+					if (current.getContent()== null)
+						variable.set_Value("0");
+					else
+						variable.set_Value(current.getContent());
+					variable.set_Variability(current.getVariability());
+					variable.set_Visibility(current.getVisibility());
+					variable.set_Flow(Boolean.valueOf(current.getFlow()));
+					twtransformer.add_Attribute(variable);
+				}
+			}
+		}
+		twtransformer.set_Stereotype(_mapPowTrans.getStereotype());
+		twtransformer.set_Package(_mapPowTrans.getPackage());
+		//for internal identification only
+		twtransformer.set_RfdId(_mapPowTrans.getRfdId());
+		
+		return twtransformer;
+	}
+	/**
+	 * Creates differnte attributes for the TwoWindingTransformer model of the Library. This attributes represent values for each winding, and
+	 * each winding in CIM is represented by different class instances, for exemple:
+	 * attribute t1 corresponds to one instance of PowerTransformerEnd, attribute t2 corresponds another instance of PowerTransformerEnd 
+	 * @param _mapPowTrans
+	 * @return
+	 */
+	public ArrayList<MOAttribute> create_AttTransformerEnd(TwoWindingTransformerMap _mapPowTrans)
+	{
+		AttributeMap current, endNumber= null;
+		MOAttribute ratioTapChanger= null, powerTransEnd= null;
+		ArrayList<MOAttribute> endAttributes= new ArrayList<MOAttribute>();
+		
+		Iterator<AttributeMap> imapAttList= _mapPowTrans.getMapAttribute().iterator();
+		while (imapAttList.hasNext()) {
+			current= imapAttList.next();
+			if (current.getCimName().equals("TransformerEnd.endNumber"))
+				endNumber= current;
+			else if (current.getCimName().equals("RatioTapChanger.stepVoltageIncrement")){
+				ratioTapChanger= this.create_TransformerEndAttribute(endNumber, current);
+				endAttributes.add(ratioTapChanger);
+			}
+			else if (current.getCimName().equals("PowerTransformerEnd.ratedU")){
+				System.out.println(current.getCimName()+ "; "+ current.getMoName());
+				powerTransEnd= this.create_TransformerEndAttribute(endNumber, current);
+				endAttributes.add(powerTransEnd);
+			}
+//			else if (current.getCimName().equals("SvVoltage.v"))
+//				svvoltage= this.create_TransformerEndAttribute(endNumber, current);
+		}
+		return endAttributes;
+	}
+	/**
+	 * Creates one attribute for each cim PowerTransformerEnd instance
+	 * @param _endNumber - Value for Power transformer ending number
+	 * @param _currentAtt - cim:RatioTapChanger.stepVoltageIncrement = mod:(t1,t2); cim:PowerTransformerEnd.ratedU = mod:(VNOM1,VNOM2)
+	 * @return modelica attribute for each instance of cim:PowerTransformerEnd
+	 */
+	private MOAttribute create_TransformerEndAttribute(AttributeMap _endNumber, AttributeMap _currentAtt) 
+	{// creates attribute t1, t2 for the twt modelica model, _currentAtt can be:
+		// RatioTapChanger.stepVoltageIncrement
+		// PowerTransformerEnd.ratedU
+		MOAttribute variable= new MOAttribute();
+		variable.set_Name(_currentAtt.getMoName()+ _endNumber.getContent());
+		if (_currentAtt.getContent()== null)
+			variable.set_Value("0");
+		else
+			variable.set_Value(_currentAtt.getContent());
+		variable.set_Variability(_currentAtt.getVariability());
+		variable.set_Visibility(_currentAtt.getVisibility());
+		variable.set_Flow(Boolean.valueOf(_currentAtt.getFlow()));
+		
+		return variable;
+	}
+	
+	public MOClass create_BusComponent(PwBusMap _mapTopoNode)
+	{
+		Bus pwbus= new Bus(_mapTopoNode.getName());
+		ArrayList<AttributeMap> mapAttList= 
+				(ArrayList<AttributeMap>)_mapTopoNode.getMapAttribute();
+		Iterator<AttributeMap> imapAttList= mapAttList.iterator();
+		AttributeMap current;
+		while (imapAttList.hasNext()) {
+			current= imapAttList.next();
+			if (current.getCimName().equals("IdentifiedObject.name")){
+				pwbus.set_InstanceName(current.getContent());
+			}
+			else {
+				MOAttribute variable= new MOAttribute();
+				variable.set_Name(current.getMoName());
+				if (current.getContent()== null)
+					variable.set_Value("0");
+				else
+					variable.set_Value(current.getContent());
+				variable.set_Variability(current.getVariability());
+				variable.set_Visibility(current.getVisibility());
+				variable.set_Flow(Boolean.valueOf(current.getFlow()));
+				pwbus.add_Attribute(variable);
+			}
+		}
+		pwbus.setStereotype(_mapTopoNode.getStereotype());
+		pwbus.set_Package(_mapTopoNode.getPackage());
+		//for internal identification only
+		pwbus.set_RfdId(_mapTopoNode.getRfdId());
+		
+		return pwbus;
+	}
+	
+	public MOClass create_FaultComponent(PwFaultMap _mapFault)
+	{
+		PwLine pwline= new PwLine(_mapFault.getName());
+		ArrayList<AttributeMap> mapAttList= 
+				(ArrayList<AttributeMap>)_mapFault.getMapAttribute();
+		Iterator<AttributeMap> imapAttList= mapAttList.iterator();
+		AttributeMap current;
+		while (imapAttList.hasNext()) {
+			current= imapAttList.next();
+			if (current.getCimName().equals("IdentifiedObject.name")){
+				pwline.set_InstanceName(current.getContent());
+			}
+			else{
+				MOAttribute variable= new MOAttribute();
+				variable.set_Name(current.getMoName());
+				if (current.getContent()== null)
+					variable.set_Value("0");
+				else
+					variable.set_Value(current.getContent());
+				variable.set_Variability(current.getVariability());
+				variable.set_Visibility(current.getVisibility());
+				variable.set_Flow(Boolean.valueOf(current.getFlow()));
+				pwline.add_Attribute(variable);
+			}
+		}
+		pwline.set_Stereotype(_mapFault.getStereotype());
+		pwline.set_Package(_mapFault.getPackage());
+		//for internal identification only
+		pwline.set_RfdId(_mapFault.getRfdId());
+		
+		return pwline;
+	}
+	
+	
+	/**
+	 * 
+	 * @param _component
+	 */
+	public void add_plantNetwork(MOPlant _plant)
+	{
+//		Iterator<MOPlant> iComponents= this.powsys.get_Equipment().iterator();
+		boolean exists= false;
+//		while (!exists && iComponents.hasNext()){
+//			exists= iComponents.next().get_InstanceName().equals(_component.get_InstanceName());
+//		}
+		if (!exists)
+			this.powsys.add_Plant(_plant);
+	}
+	
+	
+	/**
+	 * Sets the connection between all the components in the network model. 
+	 * It creates the connect equation to be written in the network model.
+	 * @param _connectmap - structure with references and ids for Terminal, ConductingEquipment and TopologicalNode
+	 */
+	public void connect_Components(ArrayList<ConnectionMap> _connectmap)
+	{
+		Iterator<ConnectionMap> iConnections= _connectmap.iterator();
+		ConnectionMap current;
+		MOClass equipment, bus; 
+		MOConnectNode conexio;
+		
+		while(iConnections.hasNext())
+		{
+			try {
+			current= iConnections.next();
+			// TODO identify machine from equipment, set machine connections
+			// TODO identify machine and controls equipment, set control connections with machine
+			equipment= this.get_equipmentNetwork(current.get_Ce_id());
+			bus= this.get_equipmentNetwork(current.get_Tn_id());
+			conexio= new MOConnectNode(equipment.get_InstanceName(), 
+					equipment.get_Terminal(current.get_T_id()).get_InstanceName(),
+					bus.get_InstanceName(), 
+					bus.get_Terminal(current.get_T_id()).get_InstanceName());
+			if (!this.powsys.exist_Connection(conexio))
+				this.powsys.add_Connection(conexio);
+			}
+			catch(NullPointerException npe)
+			{
+				System.err.println("Still some equipment left to map");
+			}
+		}
+		System.out.println(this.powsys.to_ModelicaClass());
+	}
+	
+	public void save_ModelicaFile(String _moCode)
+	{
+		BufferedWriter writer = null;
+		try {
+			String fitxer= "./model/"+ this.powsys.get_Name()+ ".mo";
+		    writer = new BufferedWriter(new FileWriter(fitxer));
+		    writer.write(_moCode);
+
+		}
+		catch ( IOException _e){
+		}
+		finally {
+		    try{
+		        if ( writer != null)
+		        writer.close( );
+		    }
+		    catch ( IOException _e){
+		    }
+		}
+	}
+}
